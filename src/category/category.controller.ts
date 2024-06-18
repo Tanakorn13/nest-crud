c11f21e2dce44ba22bba211ebb777adb1bb2bde7
Tanakorn Pongtongmuang <tanakorn.noom13@gmail.com>
2024-06-18 14:38:26 +0700

    add API tag ,API response

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags , ApiBody, ApiResponse} from '@nestjs/swagger';
 
 @ApiTags('Category')
 @Controller('category')
@@ -10,11 +10,27 @@ export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Category created successfully',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request',
+  })
+  @ApiBody({
+    description: 'Create a new category',
+    type: CreateCategoryDto
+  })
   create(@Body() createCategoryDto: CreateCategoryDto) {
     return this.categoryService.create(createCategoryDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+
+  })
   findAll() {
     return this.categoryService.findAll();
   }
